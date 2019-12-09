cb00ebf8d1d3b75ed8077e46c1e7f02901624cf6
Xu <xushengquan@souche.com>
2019-12-09 09:47:27 +0800

    Initial commit

---
+++ Chatbot_S2S_model/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChatbotS2SModelConfig(AppConfig):
+    name = 'Chatbot_S2S_model'
---
+++ Chatbot_S2S_rest/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChatbotS2SRestConfig(AppConfig):
+    name = 'Chatbot_S2S_rest'
